455bbc9afc0fea4fbe9a8a6530f9d59ad1efa05a
nvk15697 <nvk0015@gmail.com>
2022-01-06 16:20:22 +0100

    Single window

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import cv2
+import numpy as np
 
 def nothing(x):
     pass
-cv2.namedWindow('Edge Detector')
+cv2.namedWindow('Edge Tracker')
 ##creating the Trackbar
-cv2.createTrackbar('lowerThreshold','Edge Detector',0,255,nothing)
-cv2.createTrackbar('upperThreshold','Edge Detector',0,255,nothing)
+cv2.createTrackbar('lowerThreshold','Edge Tracker',0,255,nothing)
+cv2.createTrackbar('upperThreshold','Edge Tracker',0,255,nothing)
 
 cap = cv2.VideoCapture(0)
 while True:
     success, img = cap.read()
-    lowerThreshold = cv2.getTrackbarPos('lowerThreshold','Edge Detector')
-    upperThreshold = cv2.getTrackbarPos('upperThreshold','Edge Detector')
+    lowerThreshold = cv2.getTrackbarPos('lowerThreshold','Edge Tracker')
+    upperThreshold = cv2.getTrackbarPos('upperThreshold','Edge Tracker')
+    gray_scale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(img,lowerThreshold,upperThreshold)
-    cv2.imshow('Live Video', img)
-    cv2.imshow('Edge Detector',edges)
+    image = np.concatenate((gray_scale,edges),axis=1)
+    cv2.imshow('Live Stream', image)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
